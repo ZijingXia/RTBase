5dd2ad0c299bd91b70a984d24a4c98c6ef0383b5
ZijingXia <u5755597@live.warwick.ac.uk>
2026-04-27 11:44:45 +0100

    final version

--- RTBase/Main.cpp
+++ RTBase/Main.cpp
@@ -20,12 +20,12 @@ int main(int argc, char *argv[])
 	//return 0;
 	
 	// Initialize default parameters
-	std::string sceneName = "dining-room";
+	std::string sceneName = "bathroom";
 	std::string filename = "GI.hdr";
 	unsigned int SPP = 100;
 	bool enablePathTracing = true; // key to enable path tracing------------------------------------------------------------------------
 	bool enableLightTracing = false; // key to enable light tracing------------------------------------------------------------------------
-	bool enableInstantRadiosity = true; // key to enable instant radiosity----------------------------------------------------------------
+	bool enableInstantRadiosity = false; // key to enable instant radiosity----------------------------------------------------------------
 
 	if (argc > 1)
 	{
